efdb21fe4464b2a9e589c8a21507dd855acc64ab
qweshirus <qweshirus@yandex.ru>
2026-09-23 17:07:13 +0300

    Refactored

--- history/3. helloParam.cljs
+++ history/3. helloParam.cljs
@@ -1,5 +1,14 @@
 (ns cli.core)
 
+(defn get-cli-args [args]
+  "Возвращает список аргументов командной строки.
+   Если args переданы явно, использует их. Иначе читает из js/process.argv,
+   отбрасывая первые два элемента (путь к node и путь к скрипту)."
+  (or args
+      (let [argv (.-argv js/process)]
+        (when (>= (count argv) 2)
+          (vec (drop 2 argv))))))
+
 (defn print-usage []
   "Выводит подсказку по использованию приложения."
   (println "Usage: node target/main.js <your-name>")
@@ -9,16 +18,12 @@
 (defn main [& args]
   "Точка входа приложения. Принимает первый аргумент командной строки
    и выводит приветствие или ошибку."
-  (let [js-args (or args 
-                  (when (>= (count (.-argv js/process)) 2)
-                    (vec (drop 2 (.-argv js/process)))))
-      name (first js-args)]
+    (let [js-args (get-cli-args args)
+       name (first js-args)]
     (if (and nil? name (not (empty? name)))
       (do
 	    (println (str "Hello, " name "!"))
 		(js/process.exit 0))
       (do
-        (println "Error: Name is required.")
-        (println)
         (print-usage)
         (js/process.exit 1)))))
